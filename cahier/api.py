d975314d3ad6e0fa9a6ed1f85545a4c2351f6a94
RBELLI <bellirodrigo2@gmail.com>
2024-06-26 15:45:14 -0300

    ...

--- cahier/api.py
+++ cahier/api.py
@@ -1,30 +1,31 @@
 """"""
-
+from typing import Callable
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 
-from .routers.asset import router as asset_router
+from cahier.container import inject_dependency
+from cahier.interfaces.assetservice import AssetInterface
+from cahier.interfaces.assetdao import AssetDAOInterface
 
 ################################################################################
 
-def create_api(routers: list, container: dict):
+def create_api(
+    routers: list, 
+    get_asset_service: Callable[..., AssetInterface],
+    asset_service_configs: dict,
+    get_asset_dao: Callable[..., AssetDAOInterface],
+    asset_dao_configs: dict,
+    ):
     
-    #como injetar handlers ???
-    #https://python-dependency-injector.ets-labs.org/examples/fastapi.html
-
-    container.config.giphy.api_key.from_env("GIPHY_API_KEY")
+    inject_dependency(key='asset_service', dep=get_asset_service, **asset_service_configs)
+    inject_dependency(key='asset_dao', dep=get_asset_dao, **asset_dao_configs)
 
     app = FastAPI()
-    # from dependency_injector.wiring import inject, Provide
-    app.container = container
-
-    # app.include_router(asset_router)
     for router in routers:
         app.include_router(router)
 
     @app.exception_handler(HTTPException)
     async def handle_invalid_request(request, exc):
-        print("********************", exc)
         return JSONResponse(status_code=300, content=str(exc))
 
     return app
--- cahier/container.py
+++ cahier/container.py
@@ -5,20 +5,42 @@ from functools import partial
 ################################################################################
 
 container = {}
-configs = {}
+dep_configs = {}
 
-def inject(key: str, dep: Callable, **configs):
+def inject_dependency(key: str, dep: Callable, **configs)->None:
     
     if key in container:
         raise Exception()
-    
+
     container[key] = dep
-    configs[key] = configs
+    dep_configs[key] = configs
     
-def provide(key: str, **override_configs):
+def provide_dependency(key: str, **override_configs)->Callable:
     
     if key not in container:
         raise Exception()
     
-    merged_config = {**configs[key], **override_configs}
-    return partial(container[key], merged_config)
+    merged_config = {**dep_configs[key], **override_configs}
+    return partial(container[key], **merged_config)
+
+def inspect_dependencies()->list[str]:
+    return container.keys()
+
+if __name__ == '__main__':
+    
+    def get_dep1(name: str, url:str, level:int):
+        return f'{name}/{url}/{level}'
+    
+    inject_dependency('dep1', dep= get_dep1, name='RBELLI', url='cahier.com', level=10)
+    
+    depa = provide_dependency('dep1')
+    print(depa())
+    
+    depb = provide_dependency('dep1', name='NAMe2', url='NEWURL.com', level=45)
+    print(depb())
+    
+    try:
+        depc = provide_dependency('dep1', NOKEY='NAMe2', url='NEWURL.com', level=45)
+        print(depc())
+    except Exception as e:
+        print(e)
